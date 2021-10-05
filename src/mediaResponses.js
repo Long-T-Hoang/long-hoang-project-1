b949d8d67005352f7b4e7ae2cccdcd1fca56a1c4
Long Hoang <heroprotunlong@gmail.com>
2021-10-05 18:59:57 -0400

    resize logo image

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const logo = fs.readFileSync(`${__dirname}/../media/test-logo.png`);
+const logo = fs.readFileSync(`${__dirname}/../media/test-logo-small.png`);
 
 const getLogoResponse = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'image/png' });
